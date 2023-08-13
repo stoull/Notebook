0d1082ff729c9748b4ab658242da4daba537c68b
stoull <stoull@users.noreply.github.com>
2023-08-13 22:09:38 +0800

    Add crawl code

---
+++ Python/Crawl-Example-Code/CrawlUnivRanking.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup	# pip install beautifulsoup4
+from bs4
+
+def getHTMLText():
+	return ""
+
+def fillUnivList(ulist, html):
+	pass
+
+def printUnivList(ulist, num):
+	print("Suc " + str(num))
+
+def main():
+	uinfo = []
+	url = ""
